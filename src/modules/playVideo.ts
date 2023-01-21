84625164a05e1076c9754612255675da8ed874e8
Dmitry Korochkin <abramslost@gmail.com>
2023-01-21 14:09:00 +0300

    add class vldoplayer

---
+++ src/modules/playVideo.ts
@@ -0,0 +1,16 @@
+export default class VideoPlayer {
+
+
+  constructor(triggers, overlay) {
+    this.btns = document.querySelectorAll(triggers);
+    this.overlay = document.querySelector(overlay);
+    this.close = this.overlay.querySelector('.close');
+  }
+  play() {
+    this.btns.forEach(btn => {
+      btn.addEventListener('click', () => {
+        console.log('click');
+      })
+    })
+  }
+}
