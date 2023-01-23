93c5f36ac38b9a345174407c2793328b82cd63d8
Dmitry Korochkin <abramslost@gmail.com>
2023-01-23 21:07:00 +0300

    error slider.ts

--- src/modules/slider/slider-mini.ts
+++ src/modules/slider/slider-mini.ts
@@ -10,12 +10,24 @@ export default class MiniSlider extends Slider {
     super({page, next, prev});
   }
 
-  init() {
+   bindTriggers() {
+    this.next.addEventListener('click', () => {
+      this.page.append(this.slides[0]);
+    });
+    this.prev.addEventListener('click', () => {
+      let active = this.slides[this.slides.length - 1];
+      this.page.insertBefore(active, this.slides[0])
+    });
+   }
+  
+   init() {
     this.page.style.cssText = `
       display:flex;
       flex-wrap: wrap;
       overflow: hidden;
       align-items: flex-start;
     `
+
+    this.bindTriggers();
   }
 } 
--- src/modules/slider/slider.ts
+++ src/modules/slider/slider.ts
@@ -4,17 +4,34 @@ export default class Slider {
     slides: HTMLDivElement[];
     slideIndex: number;
     hanson: HTMLElement;
-    next: HTMLElement;
-    prev: HTMLElement;
+    next:  HTMLElement;
+    prev:  HTMLElement;
+    animate: HTMLElement;
+    autoplay: HTMLElement;
+    activeClass: string;
 
-  constructor({page = '', btns = '', next = '', prev = ''} = {}) {
-    this.page = document.querySelector(page) as HTMLElement;
-    this.slides = Array.from(this.page.children) as HTMLDivElement[];  //обращение к дочерним элеменnам page
-    this.btns = document.querySelectorAll(btns);
-    this.next = document.querySelector(next) as HTMLDivElement;
-    this.prev = document.querySelector(prev) as HTMLDivElement;
-    this.slideIndex = 1;
-    this.hanson = document.querySelector('.hanson') as HTMLElement;
+
+  constructor({
+    page = '', 
+    btns = '', 
+    next = '', 
+    prev = '',
+    activeClass = '',
+    animate,
+    autoplay
+} = {}) {
+
+  this.page = document.querySelector(page) as HTMLElement;
+  this.slides = Array.from(this.page.children) as HTMLDivElement[];  //обращение к дочерним элеменnам page
+  this.btns = document.querySelectorAll(btns);
+  this.next = document.querySelector(next) as HTMLDivElement;
+  this.prev = document.querySelector(prev) as HTMLDivElement;
+  this.slideIndex = 1;
+  this.hanson = document.querySelector('.hanson') as HTMLElement;
+  this.activeClass = activeClass;
+  this.animate = animate,
+  this.autoplay = autoplay,
+  
   }
 
   
