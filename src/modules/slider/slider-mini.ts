36438478b1ace2f324214bfa0b1ce0ffe28df4e3
Dmitry Korochkin <abramslost@gmail.com>
2023-01-23 19:42:35 +0300

    fix minislider

--- src/modules/slider/slider-mini.ts
+++ src/modules/slider/slider-mini.ts
@@ -1,15 +1,12 @@
 import Slider from "./slider";
 
+interface IMiniSlider {
+  page? :string, 
+  next?: string, 
+  prev?: string
+}
 export default class MiniSlider extends Slider {
-  constructor({
-    page,
-    next,
-    prev
-  } : {
-    page? :string, 
-    next?: string, 
-    prev?: string
-  }) {
+  constructor({ page, next, prev } : IMiniSlider) {
     super({page, next, prev});
   }
 
