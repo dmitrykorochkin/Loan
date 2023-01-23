094d944a4864526f0e10dcf10cde3b92e9258909
Dmitry Korochkin <abramslost@gmail.com>
2023-01-23 21:47:07 +0300

    add youtube libs

--- src/modules/playVideo.ts
+++ src/modules/playVideo.ts
@@ -1,19 +1,25 @@
+import YouTubePlayer from 'youtube-player'
+import type {YouTubePlayer as IYouTubePlayer} from 'youtube-player/dist/types';
+
 export default class VideoPlayer {
-  btns: NodeListOf<HTMLButtonElement>;
-  overlay: HTMLElement;
-  close: HTMLElement;
+  btns: NodeListOf<HTMLButtonElement>
+  overlay: HTMLElement
+  close: HTMLElement
+  player?: IYouTubePlayer
 
   constructor(triggers: string, overlay: string) {
-    this.btns = document.querySelectorAll(triggers) as NodeListOf<HTMLButtonElement>;;
-    this.overlay = document.querySelector(overlay) as HTMLElement;
-    this.close = this.overlay.querySelector('.close') as HTMLElement;
+    this.btns = document.querySelectorAll(
+      triggers
+    ) as NodeListOf<HTMLButtonElement>
+    this.overlay = document.querySelector(overlay) as HTMLElement
+    this.close = this.overlay.querySelector('.close') as HTMLElement
   }
 
   bindCloseBtn() {
     this.close.addEventListener('click', () => {
       this.overlay.style.display = 'none';
-      this.player.stopVideo();
-    });
+      (this.player as IYouTubePlayer).stopVideo()
+    })
   }
 
   bindTriggers() {
@@ -22,29 +28,22 @@ export default class VideoPlayer {
         if (document.querySelector('iframe#frame')) {
           this.overlay.style.display = 'flex'
         } else {
-          const path: string | null = btn.getAttribute('data-url');
-          this.createPlayer(path);
+          const path: string | null = btn.getAttribute('data-url')
+          this.createPlayer(path as string)
         }
-
       })
     })
   }
 
   createPlayer(url: string) {
-    this.player = new YT.Player('frame', {
+    this.player = YouTubePlayer('frame', {
       height: '100%',
       width: '100%',
-      videoId: `${url}`,
-    });
+      videoId: `${url}`
+    })
     this.overlay.style.display = 'flex '
   }
   init() {
-    const tag = document.createElement('script');
-
-    tag.src = "https://www.youtube.com/iframe_api";
-    const firstScriptTag = document.getElementsByTagName('script')[0];
-    firstScriptTag.parentNode?.insertBefore(tag, firstScriptTag);
-
     this.bindTriggers()
     this.bindCloseBtn()
   }
