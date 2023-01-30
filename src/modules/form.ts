982fc86a062cac03ccc6d032a45ed8d696de8eb0
Dmitry Korochkin <abramslost@gmail.com>
2023-01-30 16:11:01 +0300

    add forms to project

--- src/modules/form.ts
+++ src/modules/form.ts
@@ -1,6 +1,18 @@
+type MessageType = {
+  loading: string,
+  success: string,
+  failure: string
+}
+
 export default class Form {
-  constructor(forms, url) {
+  forms: NodeListOf<HTMLFormElement>
+  inputs: NodeListOf<HTMLInputElement>
+  message: MessageType
+  path: string
+
+  constructor(forms: string, url: string) {
     this.forms = document.querySelectorAll(forms);
+    this.inputs = document.querySelectorAll('input');
     this.message = {
       loading: 'Загрузка✌...',
       success: 'Спасибо, скоро мы с вами свяжемся!🎈',
@@ -9,7 +21,72 @@ export default class Form {
     this.path = '../assets/question.php';
   }
 
-  async postData(url, data) {
+  clearInputs() {
+    this.inputs.forEach(item => {
+      item.value = '';
+    })
+  }
+
+  checkMainInputs() {
+    const mailInputs: NodeListOf<HTMLInputElement> = document.querySelectorAll('[type="email"]');
+        mailInputs.forEach(input => {
+            input.addEventListener('keypress', function (e): void {
+                const key: string = e.key;
+                 if(key.match(/[^a-z 0-9 @ \.]/ig)) {
+                    e.preventDefault();
+                 }
+            })
+        })
+}
+
+initMask() {
+  const setCursorPosition = (pos: any, elem: any) => {
+        elem.focus();
+        if (elem.setSelectionRange) {
+            elem.setSelectionRange(pos, pos);
+        } else if (elem.createTextRange) {
+            let range = elem.createTextRange();
+
+            range.collapse(true);
+            range.moveEnd('character', pos);
+            range.moveStart('character', pos);
+            range.select();
+        }
+    }
+
+    function createMask (this:HTMLInputElement, e: Event): void  {
+
+        const matrix: string = `+1 (___) ___-____`;
+        let iterator: number = 0;
+        const def: string = matrix.replace(/\D/g, '');
+        let value: string = this.value.replace(/\D/g, '');
+        
+    
+        if (def.length >= value.length) {
+            value = def
+        }
+        this.value = matrix.replace(/./g, function (a: string): string {
+            return /[_\d]/.test(a) && iterator < value.length ? value.charAt(iterator++) : iterator >= value.length ? '' : a;
+        })
+
+        if (e.type === 'blur') {
+            if (this.value.length == 2) {
+                this.value = '';
+            } else {
+                setCursorPosition(this.value.length, this);
+            }
+        }
+    }
+    const inputs: NodeListOf<HTMLInputElement> = document.querySelectorAll('[name="phone"]');
+
+    inputs.forEach(input => {
+        input.addEventListener('input', createMask)
+        input.addEventListener('focus', createMask)
+        input.addEventListener('blur', createMask)
+    })
+}
+
+  async postData(url: string, data: string) {
     const res = await fetch(url, {
       method: 'POST',
       body: data
@@ -18,29 +95,38 @@ export default class Form {
     return await res.text()
   }
   init() {
+    this.initMask()
+    this.checkMainInputs()
     this.forms.forEach((form) => {
-      form.addEventListener('submit', (e) => {
+      form.addEventListener('submit', (e: Event) => {
         e.preventDefault();
 
-        const statusMessage = document.createElement('div');
+        const statusMessage: HTMLDivElement = document.createElement('div');
         statusMessage.style.cssText = `
           margin-top: 15px;
           font-size: 18px;
           color: gray;
-        `
-        form.parentNode.append(statusMessage);
+        `;
+        
+        (form as HTMLElement).parentNode?.append(statusMessage);
 
         statusMessage.textContent = this.message.loading;
 
-        const formData = new FormData(form);
-          this.postData(this.path, formData)
-            .then(res => {
-              console.log(res);
-              statusMessage.textContent = this.message.success
-            })
-            .catch(() => {
-              statusMessage.textContent = this.message.failure;
-            }
+        const formData: any = new FormData(form);
+        this.postData(this.path, formData)
+          .then(res => {
+            console.log(res);
+            statusMessage.textContent = this.message.success
+          })
+          .catch(() => {
+            statusMessage.textContent = this.message.failure
+          })
+          .finally(() => {
+            this.clearInputs();
+            setTimeout(() => {
+              statusMessage.remove();
+            }, 6000)
+          })
       })
     })
   }
