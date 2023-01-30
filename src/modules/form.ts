9f4e35558f4ff22a516f6306dd476aa08f11d3d9
Dmitry Korochkin <abramslost@gmail.com>
2023-01-30 16:11:43 +0300

    formating code

--- src/modules/form.ts
+++ src/modules/form.ts
@@ -29,62 +29,62 @@ export default class Form {
 
   checkMainInputs() {
     const mailInputs: NodeListOf<HTMLInputElement> = document.querySelectorAll('[type="email"]');
-        mailInputs.forEach(input => {
-            input.addEventListener('keypress', function (e): void {
-                const key: string = e.key;
-                 if(key.match(/[^a-z 0-9 @ \.]/ig)) {
-                    e.preventDefault();
-                 }
-            })
-        })
-}
-
-initMask() {
-  const setCursorPosition = (pos: any, elem: any) => {
-        elem.focus();
-        if (elem.setSelectionRange) {
-            elem.setSelectionRange(pos, pos);
-        } else if (elem.createTextRange) {
-            let range = elem.createTextRange();
-
-            range.collapse(true);
-            range.moveEnd('character', pos);
-            range.moveStart('character', pos);
-            range.select();
+    mailInputs.forEach(input => {
+      input.addEventListener('keypress', function (e): void {
+        const key: string = e.key;
+        if (key.match(/[^a-z 0-9 @ \.]/ig)) {
+          e.preventDefault();
         }
+      })
+    })
+  }
+
+  initMask() {
+    const setCursorPosition = (pos: any, elem: any) => {
+      elem.focus();
+      if (elem.setSelectionRange) {
+        elem.setSelectionRange(pos, pos);
+      } else if (elem.createTextRange) {
+        let range = elem.createTextRange();
+
+        range.collapse(true);
+        range.moveEnd('character', pos);
+        range.moveStart('character', pos);
+        range.select();
+      }
     }
 
-    function createMask (this:HTMLInputElement, e: Event): void  {
+    function createMask(this: HTMLInputElement, e: Event): void {
 
-        const matrix: string = `+1 (___) ___-____`;
-        let iterator: number = 0;
-        const def: string = matrix.replace(/\D/g, '');
-        let value: string = this.value.replace(/\D/g, '');
-        
-    
-        if (def.length >= value.length) {
-            value = def
-        }
-        this.value = matrix.replace(/./g, function (a: string): string {
-            return /[_\d]/.test(a) && iterator < value.length ? value.charAt(iterator++) : iterator >= value.length ? '' : a;
-        })
+      const matrix: string = `+1 (___) ___-____`;
+      let iterator: number = 0;
+      const def: string = matrix.replace(/\D/g, '');
+      let value: string = this.value.replace(/\D/g, '');
 
-        if (e.type === 'blur') {
-            if (this.value.length == 2) {
-                this.value = '';
-            } else {
-                setCursorPosition(this.value.length, this);
-            }
+
+      if (def.length >= value.length) {
+        value = def
+      }
+      this.value = matrix.replace(/./g, function (a: string): string {
+        return /[_\d]/.test(a) && iterator < value.length ? value.charAt(iterator++) : iterator >= value.length ? '' : a;
+      })
+
+      if (e.type === 'blur') {
+        if (this.value.length == 2) {
+          this.value = '';
+        } else {
+          setCursorPosition(this.value.length, this);
         }
+      }
     }
     const inputs: NodeListOf<HTMLInputElement> = document.querySelectorAll('[name="phone"]');
 
     inputs.forEach(input => {
-        input.addEventListener('input', createMask)
-        input.addEventListener('focus', createMask)
-        input.addEventListener('blur', createMask)
+      input.addEventListener('input', createMask)
+      input.addEventListener('focus', createMask)
+      input.addEventListener('blur', createMask)
     })
-}
+  }
 
   async postData(url: string, data: string) {
     const res = await fetch(url, {
@@ -107,7 +107,7 @@ initMask() {
           font-size: 18px;
           color: gray;
         `;
-        
+
         (form as HTMLElement).parentNode?.append(statusMessage);
 
         statusMessage.textContent = this.message.loading;
