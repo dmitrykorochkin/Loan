86355d180e1ab9df7467cdecbcc23ae6de1ab283
Dmitry Korochkin <abramslost@gmail.com>
2023-02-01 20:52:51 +0300

    add slider to modules.html

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,9 @@ window.addEventListener('DOMContentLoaded', () => {
   const slider = new MainSlider({ btns: '.next', page: '.page' });
   slider.render();
 
+  const modulePageSlider = new MainSlider({page: '.moduleapp', btns: 'next'});
+  modulePageSlider.render();
+
   const showUpSlider = new MiniSlider({
     page: '.showup__content-slider',
     prev: '.showup__prev',
