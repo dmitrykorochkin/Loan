4bfe360683cada93e595e9451232378a7e0a5875
Dmitry Korochkin <abramslost@gmail.com>
2023-02-06 20:51:26 +0300

    add fix type showinfo

--- src/modules/showInfo.ts
+++ src/modules/showInfo.ts
@@ -1,12 +1,13 @@
 export default class ShowInit {
+  btns: NodeListOf<HTMLButtonElement>
+
   constructor(triggers : string) {
-    this.btns = document.querySelectorAll(triggers);
+    this.btns = document.querySelectorAll(triggers) as NodeListOf<HTMLButtonElement>;
   }
   init() {
     this.btns.forEach(btn => {
       btn.addEventListener("click", () => {
-        const sibling = btn.closest('.module__info-show').nextElementSibling;
-
+        const sibling: HTMLElement = (btn.closest('.module__info-show') as HTMLElement).nextElementSibling as HTMLElement;
         sibling.classList.toggle('msg');
         sibling.style.marginTop = '20px';
       });
